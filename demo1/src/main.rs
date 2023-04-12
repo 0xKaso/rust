46a7c0c7234ce1771d56ffb6bf6f7dbe4a949130
kasodesyn <bokang.qian@desyn.io>
2023-04-12 17:31:00 +0800

    add demo1

--- demo1/src/main.rs
+++ demo1/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let url = "https://www.rust-lang.org/";
+    let output = "rust.md";
+
+    let body = reqwest::blocking::get(url).unwrap().text().unwrap();
+    let md = html2md::parse_html(&body);
+
+    std::fs::write(output, md.as_bytes()).unwrap();
+
+    
+    println!("✅ 网站抓取完成!");
 }
